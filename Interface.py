909822fe0ce194be075ace052cda074f2d735e09
worhol <trashuries@gmail.com>
2023-03-12 13:08:15 -0500

    Started working on the output in interface

--- Interface.py
+++ Interface.py
@@ -10,7 +10,7 @@ def menu(table: HashTable):
     if user_input == 2:
         time_str = input("Enter the time in military time, i.e., 0900: ")
         time_obj = datetime.strptime(time_str, '%H%M')
-        formatted_time = time_obj.strftime('%I:%M %p')
+        # formatted_time = time_obj.strftime('%I:%M %p')
 
         for i in range(len(table.data_map)):
             if table.lookup(str(i)) is not None:
@@ -19,16 +19,17 @@ def menu(table: HashTable):
 
                 if delivery_time_str is None:
                     if package_status == "at the hub":
-                        print(f"Package {i}: \033[91m{package_status}\033[0m")
+                        print(f"\033[91mPackage ID: {i}, Address: {table.lookup(str(i))[0]} {package_status}\033[0m")
                     # else:
                     #     print(f"Package {i}: en route")
                 else:
                     delivery_time_obj = datetime.strptime(delivery_time_str, '%I:%M:%S %p')
                     if delivery_time_obj.time() <= time_obj.time():
                         # print(f"Package {i}: {package_status}")
-                        print(f"\033[32mPackage {i}: {package_status}\033[0m")
+                        print(f"\033[32mPackage ID: {i}, Address: {table.lookup(str(i))[0]} {package_status} at {table.lookup(str(i))[6]}\033[0m")
                     else:
-                        print(f"\033[33mPackage {i}: en route\033[0m")
+                        print(f"\033[33mPackage ID: {i}, Address: {table.lookup(str(i))[0]} en route \033[0m")
+                        # print(f"Package ID:{i}")
 
     #     time = input("Enter the time in military time, i.e., 0900 :")
     #     time_obj = datetime.strptime(time, '%H%M')
